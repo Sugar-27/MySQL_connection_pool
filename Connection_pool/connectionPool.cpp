044407f306f6cb6457df6164839ea3c2cafb9484
sugar <45682h@gmail.com>
2022-04-24 21:37:05 +0800

    完成生产者消费者模型

--- Connection_pool/connectionPool.cpp
+++ Connection_pool/connectionPool.cpp
@@ -57,20 +57,74 @@ ConnectionPool::ConnectionPool() : m_port(3306) {
     }
     for (size_t i = 0; i < m_init_size; ++i) {
         Connection* p = new Connection();
-        p->connect(m_ip, m_user, m_password, m_dbname, m_port);
+        p->connect(m_ip, m_user, m_password, m_dbname);
+        // p->connect("localhost", "root", "root", "chat");
         m_connection_queue.push(p);
         m_connection_cnt++;
     }
+    // 启动一个生产者线程
+    thread produce(std::bind(&ConnectionPool::produce_connection, this));
+    produce.detach();
 }
 
 void ConnectionPool::print() {
-    cout << m_ip << endl;
-    cout << m_user << endl;
-    cout << m_password << endl;
-    cout << m_dbname << endl;
-    cout <<  m_port << endl;
-    cout <<  m_init_size << endl;
-    cout <<  m_max_size << endl;
-    cout <<  m_max_idletime << endl;
-    cout <<  m_timeout << endl;
+    cout << m_ip;
+    cout << m_user;
+    cout << m_password;
+    cout << m_dbname;
+    cout << m_port;
+    cout << m_init_size;
+    cout << m_max_size;
+    cout << m_max_idletime;
+    cout << m_timeout << endl;
+}
+
+// 生产者用于生产连接的函数
+void ConnectionPool::produce_connection() {
+    while (true) {
+        // 利用unique_lock在一个while循环内实现自动加锁解锁
+        std::unique_lock<std::mutex> lock(m_queue_mutex);
+        while (!m_connection_queue.empty()) {
+            // 队列内还有空闲连接
+            /* 先unlock之前获得的mutex，然后阻塞当前的执行线程。把当前线程添加到等待线程列表中，该线程会持续
+             * block 直到被 notify_all() 或 notify_one()
+             * 唤醒。被唤醒后，该thread会重新获取mutex，获取到mutex后执行后面的动作*/
+            cv.wait(lock);
+        }
+        // 总的连接数不能超过上限
+        if (m_connection_cnt >= m_max_size)
+            continue;
+        Connection* p = new Connection();
+        p->connect(m_ip, m_user, m_password, m_dbname, m_port);
+        // p->connect("localhost", "root", "root", "chat");
+        m_connection_queue.push(p);
+        ++m_connection_cnt;
+        // 生产完毕，通知可以使用
+        cv.notify_all();
+    }
+}
+
+// 消费者线程用来获取连接的函数
+std::shared_ptr<Connection> ConnectionPool::get_connection() {
+    std::unique_lock<std::mutex> lock(m_queue_mutex);
+    while (m_connection_queue.empty()) {
+        // 等待队列不空
+        if (std::cv_status::timeout ==
+            cv.wait_for(lock, std::chrono::milliseconds(m_timeout))) {
+            // 如果不是被唤醒，而是因为超时了
+            if (m_connection_queue.empty()) {
+                LOG("申请连接失败");
+                return nullptr;
+            }
+        }
+    }
+    // 使用智能指针，并重写删除器，将原始指针归还到队列里面而不是析构
+    std::shared_ptr<Connection> p(
+        m_connection_queue.front(), [this](Connection* tmp) {
+            std::unique_lock<std::mutex> lock(m_queue_mutex);
+            m_connection_queue.push(tmp);
+        });
+    m_connection_queue.pop();
+    cv.notify_all();  // 消费者取出一个，通知生产者生产
+    return p;
 }
--- Connection_pool/connectionPool.h
+++ Connection_pool/connectionPool.h
@@ -2,8 +2,11 @@
 #define CONNECTIONPOOL_H
 
 #include <atomic>
+#include <condition_variable>
 #include <cstring>
+#include <functional>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <queue>
 #include <string>
@@ -13,16 +16,19 @@
 using std::atomic;
 using std::queue;
 using std::string;
+using std::thread;
 
 class ConnectionPool {
    public:
+    // 对外部提供一个接口，用来获取单例线程池
     static ConnectionPool* get_pool();
+    // 消费者获取一个连接
+    std::shared_ptr<Connection> get_connection();
+    // 测试代码用，可删除，用于打印所有的私有成员变量
     void print();
     ~ConnectionPool() = default;
 
    private:
-    ConnectionPool();
-    bool deal_config();
     string m_ip;          // 数据库的IP
     string m_user;        // 数据库的用户名
     string m_password;    // 数据库的密码
@@ -34,7 +40,14 @@ class ConnectionPool {
     int m_timeout;        // 数据库连接池获取连接的超时时间
     // 连接队列，用于存放连接池中的所有连接
     queue<Connection*> m_connection_queue;
+    std::mutex m_queue_mutex;  // 队列锁，维护线程安全
     atomic<int> m_connection_cnt;  // 原子操作，保存现在拥有的连接数量
+    std::condition_variable cv;  // 条件变量，用于连接生产者和消费者之间的通信
+
+   private:
+    ConnectionPool();    // 单例模式，构造函数私有
+    bool deal_config();  // 处理配置文件，从配置文件加载配置项
+    void produce_connection();  // 生产者生产一个连接
 };
 
 #endif
