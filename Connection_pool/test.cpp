8b5c87c990c1c07e853457bcc624131b37c456d8
sugar <45682h@gmail.com>
2022-04-24 21:35:30 +0800

    测试代码增加对于连接池的测试

--- Connection_pool/test.cpp
+++ Connection_pool/test.cpp
@@ -1,29 +1,34 @@
 #include "connection.h"
 #include "connectionPool.h"
 
-using std::string;
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
+using std::string;
 
 int main() {
-    Connection test;
-    string sql("INSERT INTO user (name, age, sex) VALUES ('Test_user2', 1, 'female')");
+    string sql(
+        "INSERT INTO user (name, age, sex) VALUES ('Test_user2', 1, 'female')");
     cout << sql << endl;
-    if (test.connect("localhost", "root", "root", "chat")) {
-        if (test.update(sql)) {
-            cout << "更新成功\n";
-        } else {
-            cout << "更新失败\n";
-        }
-    } else {
-        cout << "连接失败\n";   
-    }
-    sql = "SELECT a FROM user";
-    test.query(sql);
-    
-    ConnectionPool* tmp = ConnectionPool::get_pool();
-    tmp->print();
+    // Connection* test = new Connection();
+    // if (test->connect("localhost", "root", "root", "chat")) {
+    //     if (test->update(sql)) {
+    //         cout << "更新成功\n";
+    //     } else {
+    //         cout << "更新失败\n";
+    //     }
+    // } else {
+    //     cout << "连接失败\n";
+    // }
+    // sql = "SELECT * FROM user";
+    // test.query(sql);
 
+    // 建立连接池，使用一个连接测试连接池是否好用
+    ConnectionPool* tmp = ConnectionPool::get_pool();
+    for (int i = 0; i < 5; ++i) {
+        std::shared_ptr<Connection> p = tmp->get_connection();
+        p->update(sql);
+    }
+    printf("结束\n");
     return 0;
 }
