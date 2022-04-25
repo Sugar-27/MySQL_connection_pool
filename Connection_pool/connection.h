d110eea032526aca590698efc9f38cc307932eed
sugar <45682h@gmail.com>
2022-04-26 01:29:56 +0800

    修复一点小bug

--- Connection_pool/connection.h
+++ Connection_pool/connection.h
@@ -2,16 +2,18 @@
 #define CONNECTION_H
 #include <mysql/mysql.h>
 #include <ctime>
-#include <string>
 #include <iostream>
+#include <string>
 
 /* 宏定义，用于打印日志 */
-#define LOG(str) std::cout << __FILE__ << ":" << __LINE__ << " " << __TIMESTAMP__ << " : " << str << std::endl;
+#define LOG(str)                                                              \
+    std::cout << __FILE__ << ":" << __LINE__ << " " << __TIMESTAMP__ << " : " \
+              << str << std::endl;
 
-using std::string;
-using std::cout;
 using std::cin;
+using std::cout;
 using std::endl;
+using std::string;
 
 /* 遵循RAII标准，对MySQL连接进行封装 */
 class Connection {
@@ -30,8 +32,17 @@ class Connection {
     bool update(string& sql);
     // 查询操作 select
     MYSQL_RES* query(string& sql);
+    // 刷新一下起始的空闲时间点
+    void refresh_alive_time() { _alive_time = clock(); }
+    // 返回存活的时间
+    clock_t get_alive_time() {
+        // 存活的时间即：当前的时间-起始的时间点
+        return clock() - _alive_time;
+    }
+
    private:
     MYSQL* _conn;
+    clock_t _alive_time;  // 记录进入空闲状态后的起始时间点(应该在进入队列的时候更新时间点)  
 };
 
 #endif
